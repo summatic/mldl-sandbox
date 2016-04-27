a4c55f2faa626992e9376cde77fce336420c279c
Hanseok Jo <summatic@gmail.com>
2016-04-27 13:18:13 +0900

    Hidden layer >= 1

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -33,6 +33,9 @@ class Layer(object):
         self.set_input_vector()
         self.set_output_vector()
 
+    def get_parameters(self):
+        return self.typ, self.node_num, self.prev_layer
+
 
 class InputLayer(Layer):
     def __init__(self, input_number, typ=None, node_num=None):
@@ -52,13 +55,15 @@ class NeuralNetwork(object):
     def __init__(self, input_data, target_data):
         self.input_vector = np.array(input_data)
         self.target_vector = np.array(target_data)
+        self.hidden_layer_list = []
 
         self.input_layer = InputLayer(input_number=self.input_vector)
-        self.hidden_layer = Layer(typ='sigm', node_num=4, prev_layer=self.input_layer)
-        self.output_layer = OutputLayer(typ='sigm', target_vector=self.target_vector, prev_layer=self.hidden_layer)
+        # self.hidden_layer = Layer(typ='sigm', node_num=4, prev_layer=self.input_layer)
+        self.output_layer = OutputLayer(typ='sigm', target_vector=self.target_vector, prev_layer=self.input_layer)
 
     def fnnpropagate(self):
-        self.hidden_layer.set_parameters()
+        for n, layer in enumerate(self.hidden_layer_list):
+            layer.set_parameters()
         self.output_layer.set_parameters()
         pass
 
@@ -98,9 +103,20 @@ class NeuralNetwork(object):
             update_matrix(self.hidden_layer)
             pass
 
+    def set_hidden_layer(self, typ, node_num):
+        prev_layer = self.hidden_layer_list[-1] if bool(self.hidden_layer_list) else self.input_layer
+        self.hidden_layer_list.append(Layer(typ, node_num, prev_layer))
+        self.output_layer.prev_layer = self.hidden_layer_list[-1]
+        pass
+
+
 input_numbers = np.array([1, 3, 4, 1])
 target_numbers = np.array([2, 1, 10])
 
 NN = NeuralNetwork(input_numbers, target_numbers)
+NN.set_hidden_layer('relu', 5)
+NN.set_hidden_layer('sigm', 10)
+NN.set_hidden_layer('relu', 7)
 NN.fnnpropagate()
-NN.fnnbackpropagate(learning_rate=0.01)
+print(NN.hidden_layer_list[0].W, NN.hidden_layer_list[1].W)
+# NN.fnnbackpropagate(learning_rate=0.01)
