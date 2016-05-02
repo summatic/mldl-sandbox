47952607998c4605287afaf6a2b77eea8f8b17e8
Hanseok Jo <summatic@gmail.com>
2016-05-03 01:10:47 +0900

    일부 주석 추가

--- FNN_keras.py
+++ FNN_keras.py
@@ -2,9 +2,11 @@
 import numpy as np
 import pandas as pd
 import glob
+from gensim.models import Phrases
 from keras.models import Sequential
 from keras.layers.embeddings import Embedding
 from keras.preprocessing import text
+from datetime import datetime
 import string
 from keras.layers.core import Dense, Activation
 
@@ -14,29 +16,74 @@ def trans():
     return f
 
 
-files = glob.glob('/Users/Hanseok/PycharmProjects/ml/test/*/*')
-files = [file for file in files if file.split('/')[-1] != 'rule']
+def read_sentences(n):
+    files = glob.glob('/Users/Hanseok/PycharmProjects/ml/test/*/*')
+    files = [file for file in files if file.split('/')[-1] != 'rule']
+    sentence_list, words_list = [], []
+    for file in files[:n]:
+        try:
+            df = pd.read_table(file)
+            sentence = ['<S> %s <E>' % row['textPattern'] for index, row in df.iterrows()]
+            sentence_list.append(sentence)
+            words = [row['textPattern'] for index, row in df.iterrows()]
+            words_list.extend(words)
+        except Exception as e:
+            continue
+    nb_words = len(list(set(words_list)))
+    return sentence_list, nb_words
 
-for file in files:
-    try:
-        df = pd.read_table(file)
-        textPattern = [row['textPattern'] for index, row in df.iterrows()]
-        docs = ' <S> '.join(textPattern)
-    except Exception as e:
-        continue
-    print(docs)
-    docs = text.one_hot(docs, n=100, filters=trans())
-    print(docs)
-    break
+
+def n_gram(n, sentence_stream):
+    """
+    :param n:
+    :param sentence_stream:
+    :return:
+    """
+    for i in range(n):
+        phrases = Phrases(sentence_stream)
+        sentence_stream = [phrases[s] for s in sentence_stream]
+    return sentence_stream
+
+
+def window(sentence_stream, return_form, window_siz):
+    """
+    :param sentence_stream:
+    :param return_form:
+    :param window_siz:
+    :return:
+    """
+    word_list = []
+    for sentence in sentence_stream:
+        s = '<S> %s <E>' % ' '.join(sentence)
+        word_list.extend(s.split())
+
+    return_list = []
+    for i in range(len(word_list) - window_siz):
+        words = word_list[i:i + window_siz]
+        words = words if return_form == 'seq' else ' '.join(words)
+        return_list.append(words)
+    return return_list
+
+
+sentences, nb_words = read_sentences(10)
+phrased = n_gram(3, sentences)
+window_size = 5
+windowed = window(phrased, 'text', window_size)
+
+print(nb_words, window_size)
+
+one_hot_list = []
+for i in windowed:
+    one_hot = text.one_hot(i, n=nb_words, filters=trans())
+    one_hot_list.append(one_hot)
 
 model = Sequential()
-model.add(Embedding(1000, 64, input_length=10000))
-# the model will take as input an integer matrix of size (batch, input_length).
-# the largest integer (i.e. word index) in the input should be no larger than 1000 (vocabulary size).
-# now model.output_shape == (None, 10, 64), where None is the batch dimension.
+model.add(Embedding(input_dim=nb_words, output_dim=15, input_length=window_size))
 
-input_array = np.random.randint(1000, size=(32, 10000))
+input_array = np.random.randint(1000, size=(32, 10))
+input_array = np.array(one_hot_list)
 
 model.compile('rmsprop', 'mse')
-output_array = model.predict(input_array)
-print(output_array.shape)
+test = input_array[0]
+print(test)
+print(model.predict(test))
